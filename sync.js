b384b7e36e8e2173865653b71398e7d87019db79
Tobias Schneider <mail@tobias-schneider.io>
2020-03-11 10:37:07 +0100

    Added error handling

--- sync.js
+++ sync.js
@@ -15,28 +15,35 @@ for (const key of keys) {
 const hosts = JSON.parse(fs.readFileSync('config/hosts.json'))
 
 for (const host of hosts) {
-    const conn = new Client()
-    conn.on('ready', () => {
-        conn.exec(`echo "${authKeys.join("\n").replace('"', '\\"')}" > ~/.ssh/authorized_keys2`, (err, stream) => {
-            if (err) {
-                console.error(`❌ ${host}`)
-                console.error(err)
-                stream.close()
-                return
-            }
-            console.log(`✅ ${host}`)
-            stream.on('close', (code, signal) => {
-                conn.end()
-            }).on('data', data => {
-                console.log(`${host}: ${data.toString().trim()}`)
-            }).stderr.on('data', (data) => {
-                console.log('STDERR: ' + data)
+    try {
+        const conn = new Client()
+        conn.on('ready', () => {
+            conn.exec(`echo "${authKeys.join("\n").replace('"', '\\"')}" > ~/.ssh/authorized_keys2`, (err, stream) => {
+                if (err) {
+                    console.error(`❌ ${host}`)
+                    // console.error(err)
+                    stream.close()
+                    return
+                }
+                console.log(`✅ ${host}`)
+                stream.on('close', (code, signal) => {
+                    conn.end()
+                }).on('data', data => {
+                    // console.log(`${host}: ${data.toString().trim()}`)
+                }).stderr.on('data', (data) => {
+                    // console.log('STDERR: ' + data)
+                })
             })
+        }).on('error', error => {
+            console.error(`❌ ${host}`)
+        }).connect({
+            host,
+            port: 22,
+            username: 'root',
+            privateKey: require('fs').readFileSync(os.homedir() + '/.ssh/id_rsa').toString(),
+            readyTimeout: 1000
         })
-    }).connect({
-        host,
-        port: 22,
-        username: 'root',
-        privateKey: require('fs').readFileSync(os.homedir() + '/.ssh/id_rsa').toString()
-    })
+    } catch (err) {
+        console.error(`❌ ${host}`)
+    }
 }
